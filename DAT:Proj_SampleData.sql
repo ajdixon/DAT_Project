a61f65b396821215bf2781faa9d4308f9d5328db
ajdixon <119520496+ajdixon@users.noreply.github.com>
2023-04-27 15:04:11 -0400

    Customer Table sample data coded by Madison

--- DAT:Proj_SampleData.sql
+++ DAT:Proj_SampleData.sql
@@ -1,1 +1,16 @@
 --Sample Data
+
+
+--Customer Table
+INSERT INTO CUSTOMER VALUES('christian@gmail.com', 
+    'Christian', 'Martinez', '11-JUL-1973', '567hello!');
+INSERT INTO CUSTOMER VALUES('jo.mcdonald@gmail.com', 
+    'Joanne', 'McDonald', '08-JAN-2001', 'tshmqr!2');
+INSERT INTO CUSTOMER VALUES('sbiles@hotmail.net', 
+    'Simone', 'Biles', '12-MAR-1995', '3olympics@');
+INSERT INTO CUSTOMER VALUES('ldemart@gmail.com', 
+    'Logan', 'Demartino', '22-FEB-2002', 'flowers33*');
+INSERT INTO CUSTOMER VALUES('moreno@villanova.edu', 
+    'Sofia', 'Moreno', '02-APR-2002', '3pepper%');
+INSERT INTO CUSTOMER VALUES('hayes.trinity@yahoo.com', 
+    'Trinity', 'Hayes', '08-JAN-2004', '5$random');

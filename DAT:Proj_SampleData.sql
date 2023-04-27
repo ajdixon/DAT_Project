a8457aba15d55c8637ba0701047c24d58f38937c
ajdixon <119520496+ajdixon@users.noreply.github.com>
2023-04-27 15:19:51 -0400

    Edited esthetician sample data

--- DAT:Proj_SampleData.sql
+++ DAT:Proj_SampleData.sql
@@ -17,28 +17,28 @@ INSERT INTO CUSTOMER VALUES('hayes.trinity@yahoo.com',
 
 
 INSERT INTO Esthetician (FName, LName, DOB, Email, Password, Description, Location)
-VALUES ('Rachel', 'Johnson', '1995-06-23', 'rachelj@gmail.com', 'pass123', 'I specialize in facials and skincare', '123 Main St, Los Angeles, CA 90001');
+VALUES ('Rachel', 'Johnson', '23-Jun-1995', 'rachelj@gmail.com', 'pass123', 'I specialize in facials and skincare', '123 Main St, Los Angeles, CA 90001');
 
 INSERT INTO Esthetician (FName, LName, DOB, Email, Password, Description, Location)
-VALUES ('Avery', 'Williams', '1993-12-07', 'awilliams@gmail.com', 'password1', 'I specialize in waxing and threading', '456 Oak St, San Francisco, CA 94102');
+VALUES ('Avery', 'Williams', '07-Dec-1993', 'awilliams@gmail.com', 'password1', 'I specialize in waxing and threading', '456 Oak St, San Francisco, CA 94102');
 
 INSERT INTO Esthetician (FName, LName, DOB, Email, Password, Description, Location)
-VALUES ('Linda', 'Nguyen', '1998-03-15', 'lnguyen@yahoo.com', 'pass456', 'I specialize in eyelash extensions', '789 Maple Ave, New York, NY 10001');
+VALUES ('Linda', 'Nguyen', '15-Mar-1998', 'lnguyen@yahoo.com', 'pass456', 'I specialize in eyelash extensions', '789 Maple Ave, New York, NY 10001');
 
 INSERT INTO Esthetician (FName, LName, DOB, Email, Password, Description, Location)
-VALUES ('Samantha', 'Chen', '1992-08-30', 'schen@hotmail.com', 'password2', 'I specialize in makeup and skincare', '234 Cherry St, Seattle, WA 98101');
+VALUES ('Samantha', 'Chen', '30-Aug-1992', 'schen@hotmail.com', 'password2', 'I specialize in makeup and skincare', '234 Cherry St, Seattle, WA 98101');
 
 INSERT INTO Esthetician (FName, LName, DOB, Email, Password, Description, Location)
-VALUES ('Emily', 'Garcia', '1997-02-14', 'emilygarcia@gmail.com', 'p@ssword!', 'I specialize in microblading and eyebrow shaping', '456 Market St, San Francisco, CA 94103');
+VALUES ('Emily', 'Garcia', '14-Feb-1997', 'emilygarcia@gmail.com', 'p@ssword!', 'I specialize in microblading and eyebrow shaping', '456 Market St, San Francisco, CA 94103');
 
 INSERT INTO Esthetician (FName, LName, DOB, Email, Password, Description, Location)
-VALUES ('Alicia', 'Lee', '1994-11-03', 'alicialee@hotmail.com', 'passw0rd', 'I specialize in chemical peels and facials', '789 Fifth Ave, New York, NY 10022');
+VALUES ('Alicia', 'Lee', '03-Nov-1994', 'alicialee@hotmail.com', 'passw0rd', 'I specialize in chemical peels and facials', '789 Fifth Ave, New York, NY 10022');
 
 INSERT INTO Esthetician (FName, LName, DOB, Email, Password, Description, Location)
-VALUES ('David', 'Kim', '1991-07-25', 'davidkim@yahoo.com', 'password123', 'I specialize in massage and reflexology', '123 Fourth St, Los Angeles, CA 90001');
+VALUES ('David', 'Kim', '25-Jul-1991', 'davidkim@yahoo.com', 'password123', 'I specialize in massage and reflexology', '123 Fourth St, Los Angeles, CA 90001');
 
 INSERT INTO Esthetician (FName, LName, DOB, Email, Password, Description, Location)
-VALUES ('Ashley', 'Brown', '1996-04-05', 'ashleynguyen@gmail.com', 'securepassword', 'I specialize in body waxing and sugaring', '234 Elm St, Seattle, WA 98102');
+VALUES ('Ashley', 'Brown', '05-Apr-1996', 'ashleynguyen@gmail.com', 'securepassword', 'I specialize in body waxing and sugaring', '234 Elm St, Seattle, WA 98102');
 
 INSERT INTO Esthetician (FName, LName, DOB, Email, Password, Description, Location)
-VALUES ('Jessica', 'Wang', '1993-10-17', 'jessicawang@gmail.com', 'p@ssword123', 'I specialize in makeup and bridal services', '567 Pine St, San Francisco, CA 94108');
+VALUES ('Jessica', 'Wang', '17-Oct-1993', 'jessicawang@gmail.com', 'p@ssword123', 'I specialize in makeup and bridal services', '567 Pine St, San Francisco, CA 94108');

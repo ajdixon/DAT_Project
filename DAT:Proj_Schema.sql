1628fa88f79bb27c3dc17c7ca8f2cadbfbabea60
ajdixon <119520496+ajdixon@users.noreply.github.com>
2023-04-27 14:58:15 -0400

    Added Customer Table-- code written by Madison

--- DAT:Proj_Schema.sql
+++ DAT:Proj_Schema.sql
@@ -10,3 +10,17 @@ CREATE TABLE Esthetician(
 	CONSTRAINT		Esthet_PK		PRIMARY KEY(Email)
 
 );
+
+DROP TABLE CUSTOMER CASCADE CONSTRAINTS;
+--DROP SEQUENCE seqCID;
+
+CREATE TABLE CUSTOMER(
+    CmrEmail        Char(50)        NOT NULL,
+    CmrFname        Char(50)        NOT NULL,
+    CmrLname        Char(50)        NOT NULL,
+    CmrBdate        Date            NULL,
+    CmrPassW        Char(15)        NOT NULL,
+    CONSTRAINT      CUSTOMER_PK        PRIMARY KEY(CmrEmail)
+    );
+
+--CREATE SEQUENCE seqCID INCREMENT BY 1 START WITH 1;

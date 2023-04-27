e85d1be278b8f2b4a709cb405f3bf61508faf3b3
isabelcevallos <130936101+isabelcevallos@users.noreply.github.com>
2023-04-27 15:38:03 -0400

    Added Reviews and Services Tables

--- DAT:Proj_Schema.sql
+++ DAT:Proj_Schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE Esthetician(
 	FName			Char(50)		NOT NULL,
 	LName			Char(50)		NOT NULL,
 	DOB			    Date			NOT NULL,
-	Email			VarChar(100)	NULL,
+	Email			VarChar(50)	NULL,
     Password        Char(50)		NOT NULL,
     Description     Char(1000)		NOT NULL,
     Location        Char(50)		NOT NULL,
@@ -24,12 +24,13 @@ CREATE TABLE CUSTOMER(
     );
 
 --CREATE SEQUENCE seqCID INCREMENT BY 1 START WITH 1;
+
 CREATE TABLE Time_Slots (
     Fdate           date        not null,
     Start_time      time        not null,
     End_time        time        not null,
-    C_email         varchar(25) not null,
-    E_email         varchar(25) not null,
+    C_email         varchar(50) not null,
+    E_email         varchar(50) not null,
     E_availability  varchar(25) not null,
     primary key (C_email, E_email),
     foreign key (C_email) references Customer(C_email),
@@ -37,3 +38,21 @@ CREATE TABLE Time_Slots (
 );
 
 
+CREATE TABLE Reviews (
+	c_email		varchar(50) not null,
+	e_email		varchar(50) not null, 
+	r_name		varchar(50), 
+	descriptions 	varchar(100), 
+	date_posted	date, 
+	stars		number(5), 
+	foreign key (c_email) references customer(email), 
+	foreign key (e_email) references esthetician(email)
+	
+);
+
+CREATE TABLE Services (
+	s_type varchar(15) not null,
+	e_email varchar(50) not null
+	
+	foreign key (e_emial) references esthetician(email)
+);

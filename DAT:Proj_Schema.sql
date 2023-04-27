9d30e60e88d757eac5a258c9d404ddcc65033391
ajdixon <119520496+ajdixon@users.noreply.github.com>
2023-04-27 15:39:36 -0400

    Edited Esthetician table

--- DAT:Proj_Schema.sql
+++ DAT:Proj_Schema.sql
@@ -2,11 +2,11 @@
 CREATE TABLE Esthetician(
 	FName			Char(50)		NOT NULL,
 	LName			Char(50)		NOT NULL,
-	DOB			    Date			NOT NULL,
-	Email			VarChar(50)	NULL,
-    Password        Char(50)		NOT NULL,
-    Description     Char(1000)		NOT NULL,
-    Location        Char(50)		NOT NULL,
+	DOB			Date			NOT NULL,
+	Email			VarChar(50)		NULL,
+   	Password        	Char(50)		NOT NULL,
+    	Description     	Char(1000)		NOT NULL,
+    	Location        	Char(50)		NOT NULL,
 	CONSTRAINT		Esthet_PK		PRIMARY KEY(Email)
 
 );

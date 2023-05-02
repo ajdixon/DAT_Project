e93a55e56abba29dbead419b23c8461700a11d15
daym4 <130935628+daym4@users.noreply.github.com>
2023-05-01 20:38:24 -0400

    made changes to all tables so that they implement and work

--- DAT:Proj_Schema.sql
+++ DAT:Proj_Schema.sql
@@ -1,17 +1,6 @@
---Schema Creation
-CREATE TABLE Esthetician(
-	FName			Char(50)		NOT NULL,
-	LName			Char(50)		NOT NULL,
-	DOB			Date			NOT NULL,
-	Email			VarChar(50)		NULL,
-   	Password        	Char(50)		NOT NULL,
-    	Description     	Char(1000)		NOT NULL,
-    	Location        	Char(50)		NOT NULL,
-	CONSTRAINT		Esthet_PK		PRIMARY KEY(Email)
 
-);
+--DROP TABLE CUSTOMER CASCADE CONSTRAINTS;
 
-DROP TABLE CUSTOMER CASCADE CONSTRAINTS;
 
 CREATE TABLE CUSTOMER(
     CmrEmail        VarChar(50)        NOT NULL,
@@ -24,34 +13,59 @@ CREATE TABLE CUSTOMER(
 
 
 
+--DROP TABLE ESTHETICIAN  CASCADE CONSTRAINTS;
+
+CREATE TABLE ESTHETICIAN(
+	FName			Char(50)		NOT NULL,
+	LName			Char(50)		NOT NULL,
+	DOB			Date			NOT NULL,
+	Email			VarChar(50)		NULL,
+   	Password        	Char(50)		NOT NULL,
+    	Description     	Char(1000)		NOT NULL,
+    	Location        	Char(50)		NOT NULL,
+	CONSTRAINT		Esthet_PK		PRIMARY KEY(Email)
+);
+
+
+
+
+
+--DROP TABLE Time_Slots CASCADE CONSTRAINTS;
+
+
 CREATE TABLE Time_Slots (
-    Fdate           date        not null,
-    Start_time      time        not null,
-    End_time        time        not null,
-    C_email         varchar(50) null,
-    E_email         varchar(50) not null,
+    Fdate           Date        not null,
+    Start_time      Char(15)        not null,
+    End_time        Char(15)        not null,
+    C_email         VarChar(50)    not null,
+    E_email         VarChar(50) not null,
     primary key (C_email, E_email),
     foreign key (C_email) references Customer(CmrEmail),
     foreign key (E_email) references Esthetician(Email) 
 );
 
 
+
+--DROP TABLE Reviews CASCADE CONSTRAINTS;
+
 CREATE TABLE Reviews (
-	c_email		varchar(50) not null,
-	e_email		varchar(50) not null, 
-	r_name		varchar(50), 
-	descriptions 	varchar(100), 
-	date_posted	date, 
+	c_email		VarChar(50) not null,
+	e_email		VarChar(50) not null, 
+	r_name		VarChar(50), 
+	descriptions 	VarChar(500), 
+	date_posted	   Date, 
 	stars		number(5), 
-	primary key (c_email, e_email)
 	foreign key (c_email) references customer(CmrEmail), 
 	foreign key (e_email) references esthetician(Email)
-	
 );
 
-CREATE TABLE Services (
-	s_type varchar(15) not null,
-	e_email varchar(50) not null
-	primary key (s_type),
-	foreign key (e_email) references esthetician(Email)
-);
+
+
+--DROP TABLE Services CASCADE CONSTRAINTS;
+
+CREATE TABLE Services(
+ 	s_type VarChar(15) not null,
+ 	e_email VarChar(50) not null,
+ 	primary key(s_type, e_email),
+ 	foreign key (e_email) references esthetician(Email)
+ );

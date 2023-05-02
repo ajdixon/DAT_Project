f8390cc629f42e3bc0e352906ddb34e278d5c2bd
daym4 <130935628+daym4@users.noreply.github.com>
2023-05-01 20:35:26 -0400

    made edits to data for all tables so that it works

--- DAT:Proj_SampleData.sql
+++ DAT:Proj_SampleData.sql
@@ -1,7 +1,5 @@
 --Sample Data
-
-
---Customer Table
+--CUSTOMER data:
 INSERT INTO CUSTOMER VALUES('christian@gmail.com', 
     'Christian', 'Martinez', '11-JUL-1973', '567hello!');
 INSERT INTO CUSTOMER VALUES('jo.mcdonald@gmail.com', 
@@ -45,13 +43,13 @@ VALUES ('Jessica', 'Wang', '17-Oct-1993', 'jessicawang@gmail.com', 'p@ssword123'
 
 
 --Reviews Table
-INSERT INTO Reviews VALUES ('christian@gmail.com', 'rachelj@gmail.com', 'Christian', 'Liked how my hair turned out, but it took a long time', 4, '01-JAN-2023');
+INSERT INTO Reviews VALUES ('christian@gmail.com', 'rachelj@gmail.com', 'Christian', 'Liked how my hair turned out, but it took a long time', '01-JAN-2023', 4);
 
-INSERT INTO Reviews VALUES ('jo.mcdonald@gmail.com', 'awilliams@gmail.com', 'Joanne', 'Best nails I have ever gotten. The gel looked great and it lasted weeks!', 5, '11-MAR-2023');
+INSERT INTO Reviews VALUES ('jo.mcdonald@gmail.com', 'awilliams@gmail.com', 'Joanne', 'Best nails I have ever gotten. The gel looked great and it lasted weeks!', '11-MAR-2023', 5);
 
-INSERT INTO Reviews VALUES ('moreno@villanova.edu', 'ashleynguyen@gmail.com', 'Sofia', 'I asked for lighter highlights and a trim and I got what I wanted. She clearly has experience but I did not like how disorganzied she was.', 3.5, '11-MAR-2023');
+INSERT INTO Reviews VALUES ('moreno@villanova.edu', 'ashleynguyen@gmail.com', 'Sofia', 'I asked for lighter highlights and a trim and I got what I wanted. She clearly has experience but I did not like how disorganzied she was.', '11-MAR-2023', 3.5);
 
-INSERT INTO Reviews VALUES ('hayes.trinity@yahoo.com', 'ashleynguyen@gmail.com', 'Trinity', 'My blond highlights had faded out so I asked to get them redone. My hair turned out yellow. Would not recommend.', 1, '15-FEB-2023');
+INSERT INTO Reviews VALUES ('hayes.trinity@yahoo.com', 'ashleynguyen@gmail.com', 'Trinity', 'My blond highlights had faded out so I asked to get them redone. My hair turned out yellow. Would not recommend.', '15-FEB-2023', 1);
 
 
 --Services Table
@@ -73,22 +71,23 @@ INSERT INTO Services VALUES ('hair', 'ashleynguyen@gmail.com');
 
 INSERT INTO Services VALUES ('nails', 'jessicawang@gmail.com');
 
+
 --Time_slots sample data
 INSERT INTO Time_slots(Fdate, Start_time, End_time, C_email, E_email)
 VALUES('01-JAN-2023','11:00:23', '12:15:07','christian@gmail.com','rachelj@gmail.com');
-INSERT INTO TIme_slots(Fdate, Start_time, End_time, C_email, E_email)
+INSERT INTO Time_slots(Fdate, Start_time, End_time, C_email, E_email)
 VALUES('03-MAR-2023','12:00:10', '13:06:66','jo.mcdonald@gmail.com','awilliams@gmail.com');
 INSERT INTO Time_slots(Fdate, Start_time, End_time, C_email, E_email)
-VALUES('09-MAR-2023','11:30:09', '12:15:07',null,'lnguyen@yahoo.com');
+VALUES('09-MAR-2023','11:30:09', '12:15:07', 'christian@gmail.com', 'lnguyen@yahoo.com');
 INSERT INTO Time_slots(Fdate, Start_time, End_time, C_email, E_email)
-VALUES('13-FEB-2023','10:00:24', '11:40:07',null,'schen@hotmail.com');
+VALUES('13-FEB-2023','10:00:24', '11:40:07','moreno@villanova.edu','schen@hotmail.com');
 INSERT INTO Time_slots(Fdate, Start_time, End_time, C_email, E_email)
 VALUES('11-MAR-2023','10:00:06', '12:03:07','moreno@villanova.edu','emilygarcia@gmail.com');
 INSERT INTO Time_slots(Fdate, Start_time, End_time, C_email, E_email)
 VALUES('04-MAR-2023','11:15:06', '14:20:07','hayes.trinity@yahoo.com','alicialee@hotmail.com');
 INSERT INTO Time_slots(Fdate, Start_time, End_time, C_email, E_email)
-VALUES('21-JAN-2023','13:06:06', '14:00:04',null,'davidkim@yahoo.com');
+VALUES('21-JAN-2023','13:06:06', '14:00:04','ldemart@gmail.com','davidkim@yahoo.com');
 INSERT INTO Time_slots(Fdate, Start_time, End_time, C_email, E_email)
-VALUES('25-JAN-2023','11:02:03', '12:04:07',null,'ashleynguyen@gmail.com');
+VALUES('25-JAN-2023','11:02:03', '12:04:07','sbiles@hotmail.net','ashleynguyen@gmail.com');
 INSERT INTO Time_slots(Fdate, Start_time, End_time, C_email, E_email)
-VALUES('17-JAN-2023','14:04:06', '15:03:07',null,'jessicawang@gmail.com');
+VALUES('17-JAN-2023','14:04:06', '15:03:07','hayes.trinity@yahoo.com','jessicawang@gmail.com');

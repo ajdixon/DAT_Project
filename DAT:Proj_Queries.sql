7a4c4ede6bc0f4cd285e7824264116ccd9f745e0
daym4 <130935628+daym4@users.noreply.github.com>
2023-05-01 20:52:13 -0400

    use case- customer deletes an account (Madison)

--- DAT:Proj_Queries.sql
+++ DAT:Proj_Queries.sql
@@ -3,3 +3,6 @@ Hello
 --use case - a customer creates an account
 INSERT INTO CUSTOMER VALUES('matt.jones@gmail.com', 
     'Matthew', 'Jones', '23-JUL-2002', '5hskfh8@');
+
+--use case- a customer deletes their account
+delete from customer where cmrfname = 'Matthew' and cmrlname = 'Jones' and cmremail= 'matt.jones@gmail.com';

e500d49615ee2d0d002e90676277705e2b515551
daym4 <130935628+daym4@users.noreply.github.com>
2023-05-03 12:07:54 -0400

    I changed and edited the use cases and wrote some

--- DAT:Proj_Queries.sql
+++ DAT:Proj_Queries.sql
@@ -1,3 +1,6 @@
+--Madison Day
+--queries from final project- esthetician
+
 
 --use cases
 --a customer can create a new profile
@@ -14,7 +17,9 @@ SELECT * FROM reviews WHERE e_email = 'rachelj@gmail.com';
 
 
 --A customer can search for all estheticians who do a specific service
-select * from esthetician where EService = 'hair';
+select fname, lname, email, EService from esthetician where EService = 'hair';
+
+
 
 
 --A customer can schedule an appointment with an Esthetician   
@@ -24,14 +29,38 @@ INSERT INTO Time_slots VALUES('18-MAY-2023', '11:30:00', '12:30:00', 'matt.jones
 
 --Estheticians are able to change their profile information
 update esthetician
-set location = '6053 S Califoria, Chicago, Illinois 60618'
+set location = '6053 S California, Chicago, Illinois 60618'
 where email = 'rachelj@gmail.com';
+
+
 
 
 --Estheticians are able to view all of their reviews
 select * from reviews
 where e_email = 'rachelj@gmail.com';
 
+
+
+
 --A customer can delete an appointment they previously scheduled 
 delete from Time_slots
-where fdate = '18-MAY-2023' and Start_time = '11:30:00' and End_time = '12:30:00' and C_email = 'matt.jones@gmail.com' and E_email = 'rachelj@gmail.com' and ApptType = 'hair';
+where fdate = '18-MAY-2023' and Start_time = '11:30:00' 
+and End_time = '12:30:00' and C_email = 'matt.jones@gmail.com' 
+and E_email = 'rachelj@gmail.com' and ApptType = 'hair';
+
+
+--customers can see the average star rating for every esthetician 
+--  who has been reviewed
+select e.email, avg(stars)
+from esthetician e right  outer join reviews b on b.e_email = e.email
+group by e.email;
+--
+
+select * from CUSTOMER;
+
+select * from ESTHETICIAN;
+
+select * from Time_Slots;
+
+select * from REVIEWS;
+
